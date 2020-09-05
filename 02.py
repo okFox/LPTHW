a4824d9b88532b5f884ca4c40411effe961e2902
okFox <smayfield@gmail.com>
2020-09-04 20:35:50 -0700

    02 complete

---
+++ 02.py
@@ -0,0 +1,12 @@
+#Is the space just for pretty??
+
+print("I could have the code like this.") # ignore me, I'm learning...
+
+# print("This won't run.")
+
+print("This will run.")
+
+""" I already googled how to 
+comment out multiple lines
+because I know it's coming and I'm 
+somewhat verbose """
