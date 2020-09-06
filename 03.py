dcf93c0929f64e602138e59a78f9d73c1f8428fc
okFox <smayfield@gmail.com>
2020-09-06 11:47:17 -0700

    names of operators

---
+++ 03.py
@@ -0,0 +1,16 @@
+# Exercise 3 - Numbers and Math
+
+"""
+The names of the characters. For use of the tongue language
+
++ : plus
+- : minus
+/ : slash
+* : asterisk
+% : percent
+< : less-than
+> : greater-than
+<= : less-than-equal
+>= : greater-than-equal
+
+"""
